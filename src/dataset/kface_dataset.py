97af52a4613107002ab80ab512c3a09d7cd3bcdf
Minhan-Bae <yuyuqw92@gmail.com>
2022-06-29 15:38:38 +0900

    feat: set image, label from csv

--- src/dataset/kface_dataset.py
+++ src/dataset/kface_dataset.py
@@ -5,23 +5,29 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 import numpy as np
+import pandas as pd
+
 
 class kfacedataset(Dataset):
-    def __init__(self, image_list, label_list, type="train", transform=None):
+    def __init__(self, data_path = "/data/komedi/dataset/540x1L_kface_nocropped/540x10L_27pt_kface.csv", type="train", transform=None):
         super().__init__()
-        self.image_list = image_list
-        self.label_list = label_list
+        self.data_path = data_path
         self.transform = transform
+        self.data_list = pd.read_csv(self.data_path,header=None).values.tolist()
         self.type = type
+    
     def __len__(self):
-        return len(self.image_list)
+        return len(self.data_list)
     
     def __getitem__(self, idx):
-        image = cv2.imread(self.image_list[idx],0)
+        image = cv2.imread(self.data_list[idx][1],0)
         
-        label = self.label_list[idx]
-        label = pd.read_csv(label).values.tolist()
-        label = np.array(label)
+        labels = self.data_list[idx][7:]
+        label_list = []
+        for label in labels:
+            x,y = eval(label[1:-1])
+            label_list.append((x,y))
+        label = np.array(label_list)
         
         if self.transform:
             transformed = self.transform(image=image, keypoints=label)
