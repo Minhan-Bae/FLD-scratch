b58fbf83130f62fb834fee404f09b7d765ef3dc5
Minhan-Bae <yuyuqw92@gmail.com>
2022-07-13 02:17:12 +0900

    feat: add scheduler

--- src/config.py
+++ src/config.py
@@ -3,23 +3,23 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 
 from models.timm_swin import timm_Net_54
-
 DEVICE = 'cuda:1' if torch.cuda.is_available() else 'cpu'
 EXP = {
     "DAY": date.today().isoformat(),
     "MODEL" : "swin",
     "EPOCH" : 200,
-    "LR" : 1e-5,
+    "LR" : 1e-4,
 }
 
 SEED = 2022
 
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 WORKERS = 4
 
-TYPE = "v4"
+TYPE = "v5"
 
 MODEL_NAME = "swin_base_patch4_window7_224"
 PRETRAINED_WEIGHT_PATH = "/data/komedi/logs/2022-07-12/swin_v2/model_logs/v2_swin_base_patch4_window7_224_200.pt"
@@ -35,5 +35,5 @@ SAVE_MODEL_PATH = f"/data/komedi/logs/{EXP['DAY']}/{EXP['MODEL']}_{TYPE}/model_l
 SAVE_MODEL = os.path.join(SAVE_MODEL_PATH, f"{TYPE}_{MODEL_NAME}_{EXP['EPOCH']}.pt")
 LOSS = nn.MSELoss()
 OPTIMIZER = optim.Adam(MODEL.parameters(), lr = EXP["LR"])
-
+SCHEDULER = CosineAnnealingWarmRestarts(OPTIMIZER, T_0=EXP["EPOCH"], T_mult=1)
 EARLY_STOPPING_CNT = 9999
