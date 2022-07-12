38ae33472c2746b5ebb8fd7b365950a5cac42564
Minhan-Bae <yuyuqw92@gmail.com>
2022-07-13 02:17:45 +0900

    feat: change valid batchsize

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -21,6 +21,6 @@ def kfacedataloader(batch_size, workers):
     train_loader = data.DataLoader(
         k_dataset_train, batch_size=batch_size, shuffle=True, num_workers=workers)
     valid_loader = data.DataLoader(
-        k_dataset_valid, batch_size=batch_size, shuffle=False, num_workers=workers)
+        k_dataset_valid, batch_size=64, shuffle=False, num_workers=workers)
     
     return train_loader, valid_loader
