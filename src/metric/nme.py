9ee793fb080b044879366d3baa38d9049a096739
Minhan-Bae <yuyuqw92@gmail.com>
2022-07-12 17:25:41 +0900

    feat: revise normalized mean error metric

--- src/metric/nme.py
+++ src/metric/nme.py
@@ -1,24 +1,27 @@
 import numpy as np
-
+from math import *
 def NME(label_pd, label_gt):
-    sum_nme = 0
-    total_cnt = 0
     label_pd = label_pd.data.cpu().numpy()
     label_gt = label_gt.data.cpu().numpy()
     
+    nme_list = []
+    length_list = []
+    
     for i in range(label_gt.shape[0]):
         landmarks_gt = label_gt[i]
-        # print(landmarks_gt)
         landmarks_pv = label_pd[i]
-        # print(landmarks_pv)
-        pupil_distance = landmarks_gt[9] - landmarks_gt[12]
-        # print(pupil_distance)
-        if pupil_distance <= 0.0:
-            pupil_distance = 0.01
-        landmarks_delta = np.linalg.norm(landmarks_pv - landmarks_gt)
-        nme = np.linalg.norm(landmarks_delta) / pupil_distance
-        sum_nme+= nme
-        total_cnt+=1
+        
+        minx, maxx = np.min(landmarks_gt), np.max(landmarks_gt)
 
-    total_nme = sum_nme/total_cnt
-    return total_nme
+        llength = (maxx - minx)
+        length_list.append(llength)
+        
+        dis = landmarks_pv - landmarks_gt
+        dis = np.sqrt(np.sum(np.power(dis,2),0))
+        nme = dis/llength
+        nme_list.append(nme)
+
+    nme_list = np.array(nme_list, dtype=np.float32)
+    mean_nme = np.mean(nme_list)*100
+    std_nme = np.std(nme_list)*100
+    return mean_nme, std_nme
