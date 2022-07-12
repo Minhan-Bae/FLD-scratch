955823efec1a1e628ac6a4375031bb7d3f62ae01
Minhan-Bae <yuyuqw92@gmail.com>
2022-07-12 18:00:21 +0900

    feat: add nme.py 2d nme calculate metric

--- src/metric/nme.py
+++ src/metric/nme.py
@@ -1,19 +1,26 @@
 import numpy as np
 from math import *
 def NME(label_pd, label_gt):
-    label_pd = label_pd.data.cpu().numpy()
-    label_gt = label_gt.data.cpu().numpy()
+    # label_pd = label_pd.data.cpu().numpy()
+    # label_gt = label_gt.data.cpu().numpy()
     
     nme_list = []
     length_list = []
     
     for i in range(label_gt.shape[0]):
-        landmarks_gt = label_gt[i]
-        landmarks_pv = label_pd[i]
         
-        minx, maxx = np.min(landmarks_gt), np.max(landmarks_gt)
+        landmarks_gt = label_gt[i].view(-1,2)
+        landmarks_gt = (landmarks_gt+0.5) * 224
+        landmarks_gt = landmarks_gt.detach().cpu().numpy()
+        
+        landmarks_pv = label_pd[i].view(-1,2)
+        landmarks_pv = (landmarks_pv+0.5) * 224
+        landmarks_pv = landmarks_pv.detach().cpu().numpy()
+        
+        minx, maxx = np.min(landmarks_gt[0,:]), np.max(landmarks_gt[0,:])
+        miny, maxy = np.min(landmarks_gt[1,:]), np.max(landmarks_gt[1,:])
 
-        llength = (maxx - minx)
+        llength = sqrt((maxx - minx) * (maxy - miny))
         length_list.append(llength)
         
         dis = landmarks_pv - landmarks_gt
