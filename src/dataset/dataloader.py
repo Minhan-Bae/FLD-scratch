f49a39eccc4a83a4d18e85c33f1addd22a85722d
Minhan-Bae <yuyuqw92@gmail.com>
2022-07-18 18:08:30 +0900

    feat: split kface,aflw valid

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -1,20 +1,29 @@
-from dataset import augmentation, aug_dataset
+from dataset import augmentation, dataset
 from torch.utils import data
 
 def kfacedataloader(batch_size, workers):
-    k_dataset_train = aug_dataset.kfacedataset(
+    k_dataset_train = dataset.kfacedataset(
         type="train",
         transform=augmentation.get_augmentation(data_type="train")
         ) # normalize만 넣어서,
 
-    k_dataset_valid = aug_dataset.kfacedataset(
-        type="valid",
+    k_dataset_valid_kface = dataset.kfacedataset(
+        type="valid_kface",
+        transform=augmentation.get_augmentation(data_type="valid")
+        )
+
+    k_dataset_valid_aflw = dataset.kfacedataset(
+        type="valid_aflw",
         transform=augmentation.get_augmentation(data_type="valid")
         )
 
     train_loader = data.DataLoader(
         k_dataset_train, batch_size=batch_size, shuffle=True, num_workers=workers)
-    valid_loader = data.DataLoader(
-        k_dataset_valid, batch_size=64, shuffle=False, num_workers=workers) #TODO 확인을 위해 shuffle=True 로 변경
     
-    return train_loader, valid_loader
+    valid_loader_kface = data.DataLoader(
+        k_dataset_valid_kface, batch_size=64, shuffle=False, num_workers=workers)
+    
+    valid_loader_aflw = data.DataLoader(
+        k_dataset_valid_aflw, batch_size=64, shuffle=False, num_workers=workers) 
+    
+    return train_loader, valid_loader_kface, valid_loader_aflw
